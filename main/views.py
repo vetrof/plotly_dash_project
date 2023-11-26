13058a3935a0234ff897983266693ed2955f1af5
Vitaly Vetrof <vetrof@gmail.com>
2023-11-26 12:09:35 +0600

    add cache

--- main/views.py
+++ main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-# init dash_app
+import functools
 from . import dash_app
 
+
+@functools.lru_cache(maxsize=None)
+def init_dash_app_cached():
+    return dash_app
+
+
 def main_page_view(request):
+    dash_app_instance = init_dash_app_cached()
     return render(request, 'index.html')
+
